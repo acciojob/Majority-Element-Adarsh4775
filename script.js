042b41eaaf843b7b6f093af75a93b6cc9879ba23
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-07 18:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+function majorityElement(nums){
+	let majorityElement = nums[0];
+	let count = 1;
+	for(let i=1; i<nums.length; i++)
+		{
+			if(nums[i] === majorityElement)
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+			
+			if(count === 0)
+			{
+				majorityElement = nums[i];
+				count = 1;
+			}
+		}
+}
+return majorityElement;
+}
